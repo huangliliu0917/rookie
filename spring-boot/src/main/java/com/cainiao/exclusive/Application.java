dcd43b88de721c0581a5d390b9fb2fb312d09350
wangkecheng <462264038@qq.com>
2018-06-27 16:37:27 +0800

    spring-boot的demo

---
+++ spring-boot/src/main/java/com/cainiao/exclusive/Application.java
@@ -0,0 +1,14 @@
+package com.cainiao.exclusive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by wangkecheng on 2018/6/27.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ spring-boot/src/main/java/com/cainiao/exclusive/controller/HelloWordController.java
@@ -0,0 +1,25 @@
+package com.cainiao.exclusive.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by wangkecheng on 2018/6/27.
+ */
+@Controller
+public class HelloWordController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    String test(String param) {
+        return param;
+    }
+
+}
